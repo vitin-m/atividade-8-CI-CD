0f7e7691431a6187bd77d3cfa9170bf00ee42a38
vitin-m <vitormelo.lopes@hotmail.com>
2023-02-13 22:23:58 -0300

    react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Teste de aplicação React.</h1>
+      <p>Com integração contínua!</p>
+    </div>
+  );
+}
